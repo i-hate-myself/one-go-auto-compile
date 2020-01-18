e608ed93fa242d51fba1b68559c627373aa8fe11
i-hate-myself <andioof@protonmail.com>
2020-01-18 17:33:41 +1000

    it was all broken so i fixed it

--- autocomp.sh
+++ autocomp.sh
@@ -3,21 +3,16 @@
 javac $(find . -name "*.java")
 #this^ compiles the java code you dont need variables to hold the names
 #because of the way javac handles names and classes
+for F in *.c; do
+  gcc -Wall -o ${F%.c} $F  && [[ `echo $?` ]] || echo "Error"
+  mv ${F%.c} ${F%.c}".out"
+done
 
-cprograms=$(find . -name "*.c")
-#this^ finds the c programs to compile
+for B in *.cpp; do
+  g++ -Wall -o ${B%.cpp} $B  && [[ `echo $?` ]] || echo "Error"
+  mv ${B%.cpp} ${B%.cpp}".out"
+done
 
-cprograms2="${cprograms//.c}"
-#this^ grabs those c files and removes the .c for the variable
-
-exec gcc $cprogram -o $cprogram2
-#this^ compiles the c programs
-
-cpprograms=$(find . -name "*.cpp")
-
-cpprograms2="${cprograms//.cpp}"
-
-exec g++ $cpprogram -o $cpprogram2
 
 #this code works the same as the gcc compiler section but with c++ programs
 #excluding c# these are the languages i work with and this is more of a personal project to shorten build times
